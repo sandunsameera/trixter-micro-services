4a470ece6b2bd65d0c96983bf76ad8102a148d28
hashandace <hashan.madushanka12m2@gmail.com>
2020-10-06 20:33:14 +0530

    Innitialize commit

---
+++ src/main/java/com/example/trixter/TrixterApplication.java
@@ -0,0 +1,13 @@
+package com.example.trixter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrixterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrixterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trixter/TrixterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trixter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrixterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
