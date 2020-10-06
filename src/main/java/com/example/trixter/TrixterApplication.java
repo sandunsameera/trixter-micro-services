6b55625ca825ccaf4f85aa37b05568f07553becb
hashandace <hashan.madushanka12m2@gmail.com>
2020-10-06 20:39:34 +0530

    innitialize

--- src/main/java/com/example/trixter/TrixterApplication.java
+++ src/main/java/com/example/trixter/TrixterApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TrixterApplication {
 
+    //starting point of application
     public static void main(String[] args) {
         SpringApplication.run(TrixterApplication.class, args);
     }
